45b0fe14da9267175cde376470a0b65dd11dae3e
KarlBox <karlbox@action-games.cz>
2026-01-08 08:12:16 +0100

    employee done

---
+++ CtrlPay/CtrlPay.Entities/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrlPay.Entities
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("user_id")]
+        private int UserId { get; set; }
+        [Column("first_name")]
+        public string FirstName { get; set; }
+        [Column("last_name")]
+        public string LastName { get; set; }
+        [Column("personal_number")]
+        public string PersonalNumber { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("phone_number")]
+        public string PhoneNumber { get; set; }
+        [Column("reports_to")]
+        private int ReportsToId { get; set; }
+        [Column("address")]
+        public string Address { get; set; }
+        [Column("postal_code")]
+        public string PostalCode { get; set; }
+        [Column("city")]
+        public string City { get; set; }
+        //[Column("photo")]
+        //public Image Image { get; set; }
+        [Column("xmr_address")]
+        public string XmrAddress { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; }
+        [ForeignKey(nameof(ReportsToId))]
+        public virtual Employee ReportsTo { get; set; }
+
+
+    }
+}
