eeee91bed96f8b74e9368aa18d4dfb3501ca4f93
KikiZC <kikizc22@gmail.com>
2026-01-08 17:43:37 +0100

    Progress?

---
+++ CtrlPay/CtrlPay.Repos/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrlPay.Repos
+{
+    public static partial class Repo
+    {
+        public static bool Login(string username, string password)
+        {
+            /*
+             * Tu logika pro přihlášení
+             *
+             * Vrací true, pokud je přihlášení úspěšné, jinak false
+             */
+            return false;
+        }
+
+        public static string LoginFailedMessage()
+        {
+            /* 
+             * Zde se vrátí message jestli je třeba heslo špatně nebo jménu atd.
+             */
+            return "Přihlášení se nezdařilo. Zkontrolujte něco co se nepovedlo.";
+        }
+    }
+}
