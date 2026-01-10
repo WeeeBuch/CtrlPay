499e13d067cc60ecd8e73d85caf2a431c502483f
KikiZC <kikizc22@gmail.com>
2026-01-10 12:33:16 +0100

    přepínání barev

---
+++ CtrlPay/CtrlPay.Avalonia/CtrlPay.Avalonia/Views/SettingsThemeView.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using CtrlPay.Avalonia.ViewModels;
+
+namespace CtrlPay.Avalonia;
+
+public partial class SettingsThemeView : UserControl
+{
+    public SettingsThemeView()
+    {
+        InitializeComponent();
+        DataContext = new SettingsThemeViewModel();
+    }
+}
