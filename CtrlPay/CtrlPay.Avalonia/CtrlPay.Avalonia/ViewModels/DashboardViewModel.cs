0200fc1fb0d899cb9e12376c8dc06ced99474f01
KikiZC <kikizc22@gmail.com>
2026-01-13 19:36:13 +0100

    Dashboard 01
    Udělány čítače kreditů a "dluhů"

---
+++ CtrlPay/CtrlPay.Avalonia/CtrlPay.Avalonia/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrlPay.Avalonia.ViewModels;
+
+public partial class DashboardViewModel : ViewModelBase
+{
+    // Instance pro první čítač
+    public CounterPieceModel TotalCredits { get; } = new()
+    {
+        Title = "Celková suma kreditů",
+        Amount = 20.000000000000m
+    };
+
+    // Instance pro druhý čítač
+    public CounterPieceModel PendingCredits { get; } = new()
+    {
+        Title = "Čekající platby",
+        Amount = 1.520000000000m
+    };
+}
