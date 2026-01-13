14056fb20276c6738b83811c858a74eb77a332df
KikiZC <kikizc22@gmail.com>
2026-01-13 20:40:22 +0100

    Dashboars 02
    hotovo podle diagarmu

--- CtrlPay/CtrlPay.Repos/ToDoRepo.cs
+++ CtrlPay/CtrlPay.Repos/ToDoRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CtrlPay.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,203 @@ namespace CtrlPay.Repos
 {
     public static class ToDoRepo
     {
-        //TODO: Metody do repos: 0
+        //TODO: Metody do repos: 2
+
+        public static List<TransactionDTO> GetTransactions(string type)
+        {
+            /* Udělat metodu, která vrátí transakce podle typu (kredity, čekající platby)
+             */
+
+            return [
+                new TransactionDTO
+                {
+                    Title = "Vklad z banky",
+                    Amount = 5.0m,
+                    Timestamp = DateTime.Now
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-2)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-2)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-2)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-10)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-10)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-10)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-10)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-10)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-10)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-10)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-10)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                },
+                new TransactionDTO
+                {
+                    Title = "Výplata odměn",
+                    Amount = 1.25m,
+                    Timestamp = DateTime.Now.AddDays(-1)
+                }
+                ];
+        }
+
+        // DTO = Data Transfer Object
+        public record TransactionDTO
+        {
+            public string Title { get; init; }
+            public decimal Amount { get; init; }
+            public DateTime Timestamp { get; init; }
+        }
     }
 }
