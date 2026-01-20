de202a0f470808d291b0c2745c9a3e4507cce991
KikiZC <kikizc22@gmail.com>
2026-01-20 08:46:09 +0100

    Začátek s QRkódama

---
+++ CtrlPay/CtrlPay.Avalonia/CtrlPay.Avalonia/ViewModels/QrCodeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CtrlPay.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtrlPay.Avalonia.ViewModels;
+
+public partial class QrCodeViewModel : ViewModelBase
+{
+    [ObservableProperty] private Bitmap? _qrCodeImage;
+    [ObservableProperty] private string _address;
+
+    public QrCodeViewModel(string address)
+    {
+        _address = address;
+        // Použijeme pomocnou třídu QrGenerator z předchozí odpovědi
+        QrCodeImage = QrGenerator.GenerateBitmap(address);
+    }
+
+    [RelayCommand]
+    private async Task CopyToClipboard()
+    {
+        var clipboard = Application.Current?.Clipboard;
+        if (clipboard != null)
+        {
+            await clipboard.SetTextAsync(Address);
+            // Zde by bylo fajn mít nějakou notifikaci, 
+            // ale pro jednoduchost teď stačí akce.
+        }
+    }
+}
