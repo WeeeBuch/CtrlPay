f1e0aad9bef9e32a8e749ed25cf3e09f34cad963
KikiZC <kikizc22@gmail.com>
2026-01-25 12:52:59 +0100

    Adds health check endpoint
    Adds a basic health check endpoint to verify that the API is running.
    This is useful for monitoring and deployment purposes.

---
+++ CtrlPay/CtrlPay.API/Controllers/HealthControler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CtrlPay.API.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthControler : ControllerBase
+{
+    [HttpGet("api")]
+    //GET : health/api
+    public IActionResult Api() => Ok("Hello there i´m alive");
+
+
+}
