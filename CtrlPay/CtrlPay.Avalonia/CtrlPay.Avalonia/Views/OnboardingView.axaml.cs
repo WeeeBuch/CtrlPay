ef82d4bbfa1b63b32dddeb4eb5587463707beeaa
KikiZC <kikizc22@gmail.com>
2026-01-26 20:22:44 +0100

    Feat(UI): Create Onboarding View and Window

---
+++ CtrlPay/CtrlPay.Avalonia/CtrlPay.Avalonia/Views/OnboardingView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CtrlPay.Avalonia;
+
+public partial class OnboardingView : UserControl
+{
+    public OnboardingView()
+    {
+        InitializeComponent();
+    }
+}
