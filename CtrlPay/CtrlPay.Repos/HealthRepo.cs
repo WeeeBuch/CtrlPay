6e50a6099b44ca91ba53cf29f1df21f333d16f4a
KikiZC <kikizc22@gmail.com>
2026-02-08 10:39:18 +0100

    opraveno health repo

--- CtrlPay/CtrlPay.Repos/HealthRepo.cs
+++ CtrlPay/CtrlPay.Repos/HealthRepo.cs
@@ -13,40 +13,30 @@ namespace CtrlPay.Repos
     {
         public static async Task<bool> TestConnectionToAPI(string connString)
         {
-
             #region Debug
             if (DebugMode.SkipApiConnectionTest)
             {
-                await Task.Delay(5000);
-
-                return string.IsNullOrWhiteSpace(connString);
+                await Task.Delay(2000);
+                return !string.IsNullOrWhiteSpace(connString);
             }
             #endregion
 
-            var handler = new HttpClientHandler
-            {
-                UseProxy = false
-            };
-
-            using HttpClient client = new(handler);
-            string uri = $"{connString}/health/api";
-
-            HttpResponseMessage response;
             try
             {
-                response = await client.GetAsync(uri);
+                string oldCreds = Credentials.BaseUri;
+
+                Credentials.BaseUri = connString;
+
+                var result = await HttpWorker.HttpGet("/health/api");
+
+                Credentials.BaseUri = oldCreds;
+                return result != null;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                AppLogger.Error("Health check connection test failed in Repo.", ex);
                 return false;
             }
-
-            if (response == null)
-            {
-                return false;
-            }
-
-            return true;
         }
     }
 }
