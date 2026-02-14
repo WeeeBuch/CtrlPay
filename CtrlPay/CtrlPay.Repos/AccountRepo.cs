008be5f085b3ae4c6290074aa95e7d5b2942368b
KarlBox <karlbox@action-games.cz>
2026-02-14 11:38:19 +0100

    Caches the credit address
    Caches the credit address to reduce the number of API calls and improve performance.
    
    Refreshes the cache if the address is empty or outdated.
    
    Related to #55

--- CtrlPay/CtrlPay.Repos/AccountRepo.cs
+++ CtrlPay/CtrlPay.Repos/AccountRepo.cs
@@ -12,7 +12,20 @@ namespace CtrlPay.Repos
 {
     public class AccountRepo
     {
+        public static string CreditAddress { get; private set; } = string.Empty;
+        public static DateTime CreditAddressLastUpdated { get; private set; } = DateTime.MinValue;
         public static async Task<string> GetCreditAddress()
+        {
+            if (CreditAddress == string.Empty || CreditAddressLastUpdated != DateTime.UtcNow)
+            {
+                AppLogger.Info("Credit address is missing or outdated. Updating credit address...");
+                CreditAddress = await UpdateCreditAddress();
+                CreditAddressLastUpdated = DateTime.UtcNow;
+            }
+            AppLogger.Info("Returning cached credit address");
+            return CreditAddress;
+        }
+        private static async Task<string> UpdateCreditAddress()
         {
             if (DebugMode.SkipCreditAddressLogic) return "DEBUG_address_for_credits";
 
