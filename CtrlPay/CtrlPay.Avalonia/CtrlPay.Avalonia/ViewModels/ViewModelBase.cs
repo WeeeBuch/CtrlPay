f624d212465772a6ea4983c9dc111577d44ae13e
KikiZC <kikizc22@gmail.com>
2026-06-06 20:44:28 +0200

    přidana avalonia

---
+++ CtrlPay/CtrlPay.Avalonia/CtrlPay.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CtrlPay.Avalonia.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ CtrlPay/CtrlPay.Avalonia/CtrlPay.Avalonia/Views/MainView.axaml.cs
@@ -0,0 +1,14 @@
+
+using Avalonia;
+using Avalonia.Controls;
+
+namespace CtrlPay.Avalonia.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
